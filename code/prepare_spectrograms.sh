f16a5ccca7ae8a865b7dd3ba4cd78ab07fc4f955
Thomas Grill <gr@grrrr.org>
2017-02-02 17:55:36 +0100

    exit instead return in bash script

--- code/prepare_spectrograms.sh
+++ code/prepare_spectrograms.sh
@@ -15,7 +15,7 @@ do
         echo "Making ${o}"
         if ! $here/extract_melspect.py --channels=mix-after -r 22050 -f 70 -l 1024 -t mel -m 50 -M 11000 -b 80 -s log --featname "features" --include-times --times-mode=borders "$f" "$o"; then
             echo "Failed making ${o} - exiting"
-            return $?
+            exit $?
         fi
     fi
 done
